5e26bbf8855504c658f1fbd0bf8f5994a8e58ba0
Allen Chou <f1470891079@gmail.com>
2024-02-03 17:28:12 +0800

    Add chat_completions endpoint to APIRouter

--- languru/server/api/v1/chat.py
+++ languru/server/api/v1/chat.py
@@ -1,9 +1,44 @@
-from fastapi import APIRouter, Request
+import time
+
+from fastapi import APIRouter, Body, Request
 from openai.types.chat.chat_completion import ChatCompletion
+
+from languru.types.chat.completions import ChatCompletionCreate
 
 router = APIRouter()
 
 
 @router.post("/completions")
-async def chat_completions(request: Request) -> ChatCompletion:
-    pass
+async def chat_completions(
+    request: Request,
+    chat_completions_create: ChatCompletionCreate = Body(
+        ...,
+        example={
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Hello!"},
+            ],
+        },
+    ),
+) -> ChatCompletion:
+    print(chat_completions_create)
+    return {
+        "id": "chatcmpl-123",
+        "object": "chat.completion",
+        "created": int(time.time()),
+        "model": "gpt-3.5-turbo-0613",
+        "system_fingerprint": "fp_44709d6fcb",
+        "choices": [
+            {
+                "index": 0,
+                "message": {
+                    "role": "assistant",
+                    "content": "\n\nHello there, how may I assist you today?",
+                },
+                "logprobs": None,
+                "finish_reason": "stop",
+            }
+        ],
+        "usage": {"prompt_tokens": 9, "completion_tokens": 12, "total_tokens": 21},
+    }
