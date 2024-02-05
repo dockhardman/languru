5540e271403cb0dea3382aa43b071ab2bea6a2b7
Allen Chou <f1470891079@gmail.com>
2024-02-05 21:56:35 +0800

    Add llm command to CLI

--- languru/cli/main.py
+++ languru/cli/main.py
@@ -11,6 +11,11 @@ def server():
     pass
 
 
+@click.group()
+def llm():
+    pass
+
+
 @click.command("run")
 def server_run():
     from languru.server.app import run_app
@@ -19,8 +24,18 @@ def server_run():
     run_app()
 
 
+@click.command("run")
+def llm_run():
+    from languru.llm.app import run_app
+
+    click.echo("Running llm server")
+    run_app()
+
+
 app.add_command(server)
+app.add_command(llm)
 server.add_command(server_run)
+llm.add_command(llm_run)
 
 
 if __name__ == "__main__":
