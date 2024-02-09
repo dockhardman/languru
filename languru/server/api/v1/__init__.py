6713f2ddfffd179a6054d7cb8a9bdfded859180d
Allen Chou <f1470891079@gmail.com>
2024-02-09 17:30:10 +0800

    Add embeddings router to API v1

--- languru/server/api/v1/__init__.py
+++ languru/server/api/v1/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from languru.server.api.v1.chat import router as chat_router
 from languru.server.api.v1.completions import router as completions_router
+from languru.server.api.v1.embeddings import router as embeddings_router
 from languru.server.api.v1.model import router as model_router
 
 router = APIRouter()
@@ -10,3 +11,4 @@ router = APIRouter()
 router.include_router(router=model_router, tags=["model"])
 router.include_router(router=chat_router, tags=["chat"])
 router.include_router(router=completions_router, tags=["completions"])
+router.include_router(router=embeddings_router, tags=["embeddings"])
---
+++ languru/server/api/v1/embeddings.py
@@ -0,0 +1,48 @@
+import math
+import random
+import time
+
+import httpx
+from fastapi import APIRouter, Body, Request
+from openai.types import CreateEmbeddingResponse
+from pyassorted.asyncio.executor import run_func
+from yarl import URL
+
+from languru.resources.model.discovery import ModelDiscovery
+from languru.server.config import settings
+from languru.types.embeddings import EmbeddingRequest
+
+router = APIRouter()
+
+
+@router.post("/embeddings")
+async def text_completions(
+    request: Request,
+    embedding_request: EmbeddingRequest = Body(
+        ...,
+        example={
+            "input": "The food was delicious and the waiter...",
+            "model": "text-embedding-ada-002",
+            "encoding_format": "float",
+        },
+    ),
+) -> CreateEmbeddingResponse:
+    if getattr(request.app.state, "model_discovery", None) is None:
+        raise ValueError("Model discovery is not initialized")
+    model_discovery: "ModelDiscovery" = request.app.state.model_discovery
+    models = await run_func(
+        model_discovery.list,
+        id=embedding_request.model,
+        created_from=math.floor(time.time() - settings.MODEL_REGISTER_PERIOD),
+    )
+    if len(models) == 0:
+        raise ValueError(f"Model '{embedding_request.model}' not found")
+
+    model = random.choice(models)
+    url = URL(model.owned_by).with_path("/embeddings")
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            str(url), json=embedding_request.model_dump(exclude_none=True)
+        )
+        response.raise_for_status()
+        return CreateEmbeddingResponse(**response.json())
