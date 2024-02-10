90961b537962b5308a76d16f8087699366392b84
Allen Chou <f1470891079@gmail.com>
2024-02-10 20:09:41 +0800

    Refactor ModerationRequest model in moderations.py

--- languru/types/moderations.py
+++ languru/types/moderations.py
@@ -1,13 +1,14 @@
-from typing import List, Literal, Optional, Text, Union
+from typing import List, Optional, Text, Union
 
 from pydantic import BaseModel, Field
 
 
 class ModerationRequest(BaseModel):
     input: Union[Text, List[Text]]
-    model: Optional[Literal["text-moderation-stable", "text-moderation-latest"]] = (
-        Field(
-            default="text-moderation-latest",
-            description="Two content moderations models are available: text-moderation-stable and text-moderation-latest.",
-        )
+    model: Optional[Text] = Field(
+        default="text-moderation-latest",
+        description=(
+            "Two content moderations models are available: "
+            + "text-moderation-stable and text-moderation-latest."
+        ),
     )
