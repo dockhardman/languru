cb937d1016d0df7713afefa3898a919fc2523376
Allen Chou <f1470891079@gmail.com>
2024-02-13 20:24:51 +0800

    Update type hints in common.py

--- languru/utils/common.py
+++ languru/utils/common.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional, Text, TypeVar, List, Tuple
+from typing import Any, List, Optional, Text, Tuple, TypeVar
 
 from languru.config import logger
 
@@ -22,10 +22,20 @@ def should_str(value: Text | Any) -> Text:
 
 def must_list_or_none(value: List[T] | Any) -> Optional[List[T]]:
     if isinstance(value, List):
+        if len(value) == 0:
+            return None
         return value
     elif isinstance(value, Tuple):
+        if len(value) == 0:
+            return None
         return list(value)
     elif value is None:
         return None
     else:
         return [value]
+
+
+def must_list(value: List[T] | Any) -> List[T]:
+    if must_list_or_none(value) is None:
+        raise ValueError(f"Could not convert {value} to a list")
+    return value
