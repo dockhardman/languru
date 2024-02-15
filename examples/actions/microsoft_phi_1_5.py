6785cad70170d18d8c6b0beb62cad91208e4a33f
Allen Chou <f1470891079@gmail.com>
2024-02-15 19:01:32 +0800

    Add Microsoft Phi 1.5 text completion action

---
+++ examples/actions/microsoft_phi_1_5.py
@@ -0,0 +1,46 @@
+from textwrap import dedent
+
+import torch
+from rich import print
+
+from languru.action.base import ActionBase
+from languru.action.hf import TransformersAction
+from languru.types.completions import CompletionRequest
+from languru.utils.device import validate_device
+
+torch.set_default_device(validate_device())
+
+
+def run_action_text_completion(action: "ActionBase"):
+    completion_params = CompletionRequest.model_validate(
+        {
+            "model": "microsoft/phi-1_5",
+            "prompt": dedent(
+                """
+                Alice: I don't know why, I'm struggling to maintain focus while studying. Any suggestions?
+                Bob: Well, have you tried creating a study schedule and sticking to it?
+                Alice: Yes, I have, but it doesn't seem to help much.
+                Bob: Hmm, maybe you should try studying in a quiet environment, like the library.
+                Alice:
+                """
+            ).strip(),
+            "max_tokens": 200,
+            "stop": ["\nAlice:", "\nBob:"],
+        }
+    )
+    completion_res = action.text_completion(
+        **completion_params.model_dump(exclude_none=True)
+    )
+    print(completion_res.model_dump())
+
+
+class MicrosoftPhi2Action(TransformersAction):
+    # Model configuration
+    MODEL_NAME = "microsoft/phi-1_5"
+
+
+if __name__ == "__main__":
+
+    action = MicrosoftPhi2Action()
+
+    run_action_text_completion(action)
