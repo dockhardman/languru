067c47e9e7aba4689bbc676493a351c628cf8761
Allen Chou <f1470891079@gmail.com>
2024-02-18 12:19:48 +0800

    Add st_MiniLM_L12.py for SentenceTransformersParaphraseMultilingualMiniLML12V2Action

---
+++ examples/actions/st_MiniLM_L12.py
@@ -0,0 +1,23 @@
+from rich import print
+
+from languru.action.base import ModelDeploy
+from languru.action.hf import TransformersAction
+
+
+class SentenceTransformersParaphraseMultilingualMiniLML12V2Action(TransformersAction):
+    # Model configuration
+    MODEL_NAME = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+
+    model_deploys = (
+        ModelDeploy(MODEL_NAME, MODEL_NAME),
+        ModelDeploy("MiniLM-L12-v2", MODEL_NAME),
+    )
+
+
+if __name__ == "__main__":
+
+    action = SentenceTransformersParaphraseMultilingualMiniLML12V2Action()
+    print(f"Health: {action.health()}")
+
+    res = action.embeddings("Hello, world!", model=action.model_name)
+    print(res.model_dump(exclude={"data": {-1: {"embedding"}}}))
