a1057c0fae342d00fa2a64d347c1d5f253033acb
Allen Chou <f1470891079@gmail.com>
2024-03-05 12:47:14 +0000

    Add token rate and GPU memory usage metrics

--- examples/actions/google_gemma_quant.py
+++ examples/actions/google_gemma_quant.py
@@ -1,8 +1,12 @@
-from typing import Text
 import time
+from typing import Text
+
+import torch
 from rich import print
 
 from languru.action.hf import TransformersAction
+
+run_times = 3
 
 
 class GoogleGemmaChatAction(TransformersAction):
@@ -18,9 +22,9 @@ class GoogleGemmaChatAction(TransformersAction):
 
 def chat(**kwargs):
     action = GoogleGemmaChatAction(**kwargs)
-    print(f"Health: {action.health()}")
+    assert action.health() is True
 
-    start = time.time()
+    # Init model
     chat_params = {
         "model": action.MODEL_NAME,
         "messages": [
@@ -29,14 +33,29 @@ def chat(**kwargs):
         "max_tokens": 200,
     }
     chat_res = action.chat(**chat_params)
+
+    start = time.time()
+    total_completion_tokens = 0
+    for _ in range(run_times):
+        chat_res = action.chat(**chat_params)
+        if chat_res.usage:
+            total_completion_tokens += chat_res.usage.completion_tokens
     timecost = time.time() - start
-    if chat_res.usage:
-        print(
-            f"Tokens Rate: {chat_res.usage.completion_tokens / timecost:.2f} "
-            + f"tokens/s, in config: {kwargs}"
-        )
+    print(
+        f"Tokens Rate: {total_completion_tokens / timecost:.2f} "
+        + f"tokens/s, in config: {kwargs}"
+    )
+    # GPU ram usage
+    if torch.cuda.is_available():
+        allocated_bytes = torch.cuda.memory_allocated("cuda:0")
+        total_bytes = torch.cuda.get_device_properties("cuda:0").total_memory
+        allocated_gb = allocated_bytes / 1024**3
+        total_gb = total_bytes / 1024**3
+        memory_usage_str = f"{allocated_gb:.2f}/{total_gb:.2f} GB"
+        print(f"GPU memory usage: {memory_usage_str}")
 
 
 if __name__ == "__main__":
+    chat(use_quantization=False, torch_dtype=torch.bfloat16)
     chat(load_in_8bit=True)
     chat(load_in_4bit=True)
