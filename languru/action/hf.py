b6bc39105967bed54b7f76b64f84f657de8b3986
Allen Chou <f1470891079@gmail.com>
2024-03-05 11:58:41 +0000

    Add model quantization configuration

--- languru/action/hf.py
+++ languru/action/hf.py
@@ -18,6 +18,7 @@ from transformers import (
     AutoModel,
     AutoModelForCausalLM,
     AutoTokenizer,
+    BitsAndBytesConfig,
     PreTrainedModel,
     PreTrainedTokenizer,
     PreTrainedTokenizerFast,
@@ -55,6 +56,20 @@ class TransformersAction(ActionBase):
         ModelDeploy(MODEL_NAME.split("/")[-1], MODEL_NAME),
     )
 
+    # Model Quantization configuration
+    use_quantization: bool = bool(
+        os.getenv("HF_USE_QUANTIZATION") or os.getenv("USE_QUANTIZATION") or False
+    )
+    load_in_8bit: Optional[bool] = None
+    load_in_4bit: Optional[bool] = None
+    llm_int8_threshold: Optional[float] = None
+    llm_int8_skip_modules: Optional[Sequence[Text]] = None
+    llm_int8_enable_fp32_cpu_offload: Optional[bool] = None
+    llm_int8_has_fp16_weight: Optional[bool] = None
+    bnb_4bit_compute_dtype: Optional[Text] = None
+    bnb_4bit_quant_type: Optional[Text] = None
+    bnb_4bit_use_double_quant: Optional[bool] = None
+
     # Generation configuration
     stop_words: Sequence[Text] = ()
     is_causal_lm: bool = True
@@ -66,12 +81,7 @@ class TransformersAction(ActionBase):
         self.device = DEVICE
 
         # Model name
-        self.model_name = (
-            should_str_or_none(kwargs.get("model_name")) or self.MODEL_NAME
-        )
-        logger.info(f"Using model: {self.model_name}")
-        if not self.model_name:
-            raise ValueError("The `model_name` cannot be empty")
+        self.model_name = self.read_model_name(**kwargs)
         # Model and tokenizer
         if self.is_causal_lm is True:
             self.model: "PreTrainedModel" = AutoModelForCausalLM.from_pretrained(
@@ -79,6 +89,7 @@ class TransformersAction(ActionBase):
                 device_map=self.device,
                 torch_dtype=self.dtype,
                 trust_remote_code=True,
+                quantization_config=self.load_quantization_config(**kwargs),
             )
         else:
             self.model: "PreTrainedModel" = AutoModel.from_pretrained(
@@ -329,6 +340,87 @@ class TransformersAction(ActionBase):
                 f"Input is not a tensor, converting to tensor: {type(input)}"
             )
         return input
+
+    def read_model_name(self, **kwargs) -> Text:
+        model_name = should_str_or_none(kwargs.get("model_name")) or self.MODEL_NAME
+        if not model_name:
+            raise ValueError("The `model_name` cannot be empty")
+        logger.info(f"Using model: {model_name}")
+        return model_name
+
+    def load_quantization_config(self, **kwargs) -> Optional["BitsAndBytesConfig"]:
+        use_quantization = bool(kwargs.get("use_quantization") or self.use_quantization)
+        if use_quantization is True:
+            params = {}
+            load_in_8bit = (
+                kwargs["load_in_8bit"]
+                if "load_in_8bit" in kwargs
+                else self.load_in_8bit
+            )
+            load_in_4bit = (
+                kwargs["load_in_4bit"]
+                if "load_in_4bit" in kwargs
+                else self.load_in_4bit
+            )
+            llm_int8_threshold = (
+                kwargs["llm_int8_threshold"]
+                if "llm_int8_threshold" in kwargs
+                else self.llm_int8_threshold
+            )
+            llm_int8_skip_modules = (
+                kwargs["llm_int8_skip_modules"]
+                if "llm_int8_skip_modules" in kwargs
+                else self.llm_int8_skip_modules
+            )
+            llm_int8_enable_fp32_cpu_offload = (
+                kwargs["llm_int8_enable_fp32_cpu_offload"]
+                if "llm_int8_enable_fp32_cpu_offload" in kwargs
+                else self.llm_int8_enable_fp32_cpu_offload
+            )
+            llm_int8_has_fp16_weight = (
+                kwargs["llm_int8_has_fp16_weight"]
+                if "llm_int8_has_fp16_weight" in kwargs
+                else self.llm_int8_has_fp16_weight
+            )
+            bnb_4bit_compute_dtype = (
+                kwargs["bnb_4bit_compute_dtype"]
+                if "bnb_4bit_compute_dtype" in kwargs
+                else self.bnb_4bit_compute_dtype
+            )
+            bnb_4bit_quant_type = (
+                kwargs["bnb_4bit_quant_type"]
+                if "bnb_4bit_quant_type" in kwargs
+                else self.bnb_4bit_quant_type
+            )
+            bnb_4bit_use_double_quant = (
+                kwargs["bnb_4bit_use_double_quant"]
+                if "bnb_4bit_use_double_quant" in kwargs
+                else self.bnb_4bit_use_double_quant
+            )
+            if load_in_8bit is not None:
+                params["load_in_8bit"] = load_in_8bit
+            if load_in_4bit is not None:
+                params["load_in_4bit"] = load_in_4bit
+            if llm_int8_threshold is not None:
+                params["llm_int8_threshold"] = llm_int8_threshold
+            if llm_int8_skip_modules is not None:
+                params["llm_int8_skip_modules"] = llm_int8_skip_modules
+            if llm_int8_enable_fp32_cpu_offload is not None:
+                params["llm_int8_enable_fp32_cpu_offload"] = (
+                    llm_int8_enable_fp32_cpu_offload
+                )
+            if llm_int8_has_fp16_weight is not None:
+                params["llm_int8_has_fp16_weight"] = llm_int8_has_fp16_weight
+            if bnb_4bit_compute_dtype is not None:
+                params["bnb_4bit_compute_dtype"] = bnb_4bit_compute_dtype
+            if bnb_4bit_quant_type is not None:
+                params["bnb_4bit_quant_type"] = bnb_4bit_quant_type
+            if bnb_4bit_use_double_quant is not None:
+                params["bnb_4bit_use_double_quant"] = bnb_4bit_use_double_quant
+            logger.info(f"Using quantization config: {params}")
+            return BitsAndBytesConfig(**params)
+        else:
+            return None
 
     def remove_echo_text(
         self,
