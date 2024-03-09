260c490b3d700658e37348ffb52a10d1eb499dec
Allen Chou <f1470891079@gmail.com>
2024-03-09 11:40:58 +0800

    Add GroqAction class and update GroqOpenaiAction class

--- languru/action/groq.py
+++ languru/action/groq.py
@@ -8,6 +8,39 @@ from languru.action.openai import OpenaiAction
 from languru.llm.config import logger
 
 
+class GroqAction(OpenaiAction):
+    model_deploys = (
+        ModelDeploy("llama2-70b-4096", "llama2-70b-4096"),
+        ModelDeploy("mixtral-8x7b-32768", "mixtral-8x7b-32768"),
+    )
+
+    def __init__(
+        self,
+        *args,
+        api_key: Optional[Text] = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        from groq import Groq
+
+        api_key = api_key or os.environ.get("GROQ_API_KEY")
+        if api_key is None:
+            logger.error("GROQ_API_KEY is not set")
+        self._client = Groq(api_key=api_key)
+
+    def name(self):
+        return "groq_action"
+
+    def health(self) -> bool:
+        try:
+            self._client.models.retrieve(model="mixtral-8x7b-32768")
+            return True
+        except Exception as e:
+            logger.error(f"OpenAI health check failed: {e}")
+            return False
+
+
 class GroqOpenaiAction(OpenaiAction):
     model_deploys = (
         ModelDeploy("llama2-70b-4096", "llama2-70b-4096"),
