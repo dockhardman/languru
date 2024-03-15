f043888acc7264fdaa73b4843bdc2bb6b545e69c
Allen Chou <f1470891079@gmail.com>
2024-03-15 14:58:05 +0800

    refactor(chat.py): add ChatCompletionHandler to handle chat completions
    - Add ChatCompletionHandler class to handle chat completion requests
    - Handle chat completions for both LLM and Agent applications
    - Handle streaming and non-streaming responses
    - Refactor code to use new handler
    - Import required modules and classes

--- languru/server/api/v1/chat.py
+++ languru/server/api/v1/chat.py
@@ -1,26 +1,159 @@
 import math
 import random
 import time
+from typing import cast
 
 import httpx
-from fastapi import APIRouter, Body, HTTPException, Request
+from fastapi import APIRouter, Body, Depends, HTTPException, Request
 from fastapi.responses import StreamingResponse
-from openai.types.chat.chat_completion import ChatCompletion
-from pyassorted.asyncio.executor import run_func
+from openai.types.chat import ChatCompletion, ChatCompletionChunk
+from pyassorted.asyncio.executor import run_func, run_generator
 from yarl import URL
 
-from languru.resources.model.discovery import ModelDiscovery
-from languru.server.config_agent import settings
+from languru.exceptions import ModelNotFound
+from languru.server.config import (
+    AgentSettings,
+    AppType,
+    LlmSettings,
+    ServerBaseSettings,
+)
+from languru.server.deps.common import app_settings
+from languru.server.utils.common import get_value_from_app
 from languru.types.chat.completions import ChatCompletionRequest
 from languru.utils.http import requests_stream_lines
 
 router = APIRouter()
 
 
+class ChatCompletionHandler:
+
+    async def handle_request(
+        self,
+        request: "Request",
+        chat_completion_request: "ChatCompletionRequest",
+        settings: "ServerBaseSettings",
+        **kwargs,
+    ) -> ChatCompletion | ChatCompletionChunk | StreamingResponse:
+        if settings.APP_TYPE == AppType.llm:
+            settings = cast(LlmSettings, settings)
+            return await self.handle_llm(
+                request=request,
+                chat_completion_request=chat_completion_request,
+                settings=settings,
+                **kwargs,
+            )
+
+        if settings.APP_TYPE == AppType.agent:
+            settings = cast(AgentSettings, settings)
+            return await self.handle_agent(
+                request=request,
+                chat_completion_request=chat_completion_request,
+                settings=settings,
+                **kwargs,
+            )
+
+        # Not implemented or unknown app server type
+        raise HTTPException(
+            status_code=500,
+            detail=(
+                f"Unknown app server type: {settings.APP_TYPE}"
+                if settings.APP_TYPE
+                else "App server type not implemented"
+            ),
+        )
+
+    async def handle_llm(
+        self,
+        request: "Request",
+        chat_completion_request: "ChatCompletionRequest",
+        settings: "LlmSettings",
+        **kwargs,
+    ) -> ChatCompletion | ChatCompletionChunk | StreamingResponse:
+        from languru.action.base import ActionBase
+
+        action: "ActionBase" = get_value_from_app(
+            request.app, key="action", value_typing=ActionBase
+        )
+
+        try:
+            chat_completion_request.model = action.get_model_name(
+                chat_completion_request.model
+            )
+        except ModelNotFound as e:
+            raise HTTPException(status_code=404, detail=str(e))
+
+        # Stream
+        if chat_completion_request.stream is True:
+            return StreamingResponse(
+                run_generator(
+                    action.chat_stream_sse,
+                    **chat_completion_request.model_dump(exclude_none=True),
+                ),
+                media_type="application/stream+json",
+            )
+
+        # Normal
+        else:
+            chat_completion = await run_func(
+                action.chat, **chat_completion_request.model_dump(exclude_none=True)
+            )
+            return chat_completion
+
+    async def handle_agent(
+        self,
+        request: "Request",
+        chat_completion_request: "ChatCompletionRequest",
+        settings: "AgentSettings",
+        **kwargs,
+    ) -> ChatCompletion | ChatCompletionChunk | StreamingResponse:
+        from languru.resources.model.discovery import ModelDiscovery
+
+        model_discovery: "ModelDiscovery" = get_value_from_app(
+            request.app, key="model_discovery", value_typing=ModelDiscovery
+        )
+
+        # Get model name and model destination
+        models = await run_func(
+            model_discovery.list,
+            id=chat_completion_request.model,
+            created_from=math.floor(time.time() - settings.MODEL_REGISTER_PERIOD),
+        )
+        if len(models) == 0:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Model '{chat_completion_request.model}' not found",
+            )
+        model = random.choice(models)
+        url = URL(model.owned_by).with_path("/chat/completions")
+
+        # Request completion
+        # Stream
+        if chat_completion_request.stream is True:
+            return StreamingResponse(
+                requests_stream_lines(
+                    str(url),
+                    data=chat_completion_request.model_dump(exclude_none=True),
+                ),
+                media_type="application/stream+json",
+            )
+        # Normal
+        else:
+            async with httpx.AsyncClient(timeout=30) as client:
+                response = await client.post(
+                    str(url),
+                    json=chat_completion_request.model_dump(exclude_none=True),
+                )
+                response.raise_for_status()
+                return ChatCompletion(**response.json())
+
+
+chat_completion_handler = ChatCompletionHandler()
+
+
 @router.post("/chat/completions")
 async def chat_completions(
     request: Request,
-    chat_completions_request: ChatCompletionRequest = Body(
+    chat_completion_request: ChatCompletionRequest = Body(
         ...,
         example={
             "model": "gpt-3.5-turbo",
@@ -30,72 +163,10 @@ async def chat_completions(
             ],
         },
     ),
+    settings: ServerBaseSettings = Depends(app_settings),
 ):  # -> openai.types.chat.ChatCompletion | openai.types.chat.ChatCompletionChunk
-    if getattr(request.app.state, "model_discovery", None) is None:
-        raise ValueError("Model discovery is not initialized")
-
-    # Get model name and model destination
-    model_discovery: "ModelDiscovery" = request.app.state.model_discovery
-    models = await run_func(
-        model_discovery.list,
-        id=chat_completions_request.model,
-        created_from=math.floor(time.time() - settings.MODEL_REGISTER_PERIOD),
+    return await chat_completion_handler.handle_request(
+        request=request,
+        chat_completion_request=chat_completion_request,
+        settings=settings,
     )
-    if len(models) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Model '{chat_completions_request.model}' not found",
-        )
-    model = random.choice(models)
-    url = URL(model.owned_by).with_path("/chat/completions")
-
-    # Request completion
-    # Stream
-    if chat_completions_request.stream is True:
-        return StreamingResponse(
-            requests_stream_lines(
-                str(url), data=chat_completions_request.model_dump(exclude_none=True)
-            ),
-            media_type="application/stream+json",
-        )
-    # Normal
-    else:
-        async with httpx.AsyncClient(timeout=30) as client:
-            response = await client.post(
-                str(url), json=chat_completions_request.model_dump(exclude_none=True)
-            )
-            response.raise_for_status()
-            return ChatCompletion(**response.json())
-
-
-# @app.post("/chat/completions")
-# async def chat_completions(
-#     request: Request,
-#     chat_completion_request: ChatCompletionRequest = Body(...),
-# ):  # -> openai.types.chat.ChatCompletion | openai.types.chat.ChatCompletionChunk
-#     if getattr(request.app.state, "action", None) is None:
-#         raise ValueError("Action is not initialized")
-#     action: "ActionBase" = request.app.state.action
-#     try:
-#         chat_completion_request.model = action.get_model_name(
-#             chat_completion_request.model
-#         )
-#     except ModelNotFound as e:
-#         raise HTTPException(status_code=404, detail=str(e))
-
-#     # Stream
-#     if chat_completion_request.stream is True:
-#         return StreamingResponse(
-#             run_generator(
-#                 action.chat_stream_sse,
-#                 **chat_completion_request.model_dump(exclude_none=True),
-#             ),
-#             media_type="application/stream+json",
-#         )
-
-#     # Normal
-#     else:
-#         chat_completion = await run_func(
-#             action.chat, **chat_completion_request.model_dump(exclude_none=True)
-#         )
-#         return chat_completion
