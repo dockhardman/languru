0345d835eb4572d05f21c149225ddce04dc058ce
Allen Chou <f1470891079@gmail.com>
2024-05-04 21:33:07 +0800

    ENH: Improve common utility functions
    - Add type hints and docstrings for `ensure_openai_chat_completion_content` and `ensure_openai_chat_completion_message_params`
    - Extend `ensure_openai_chat_completion_message_params` to handle various message types and validate roles
    - Update import statements for better readability and organization

--- languru/utils/common.py
+++ languru/utils/common.py
@@ -1,7 +1,19 @@
 import json
 import string
-from typing import TYPE_CHECKING, Any, List, Optional, Sequence, Text, Tuple, TypeVar
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    List,
+    Optional,
+    Sequence,
+    Text,
+    Tuple,
+    TypeVar,
+    Union,
+)
 
+from openai.types.chat import ChatCompletionMessageParam
 from pydantic import BaseModel
 from rich import box
 from rich import print as rich_print
@@ -10,6 +22,7 @@ from rich.table import Table
 from rich.text import Text as RichText
 
 from languru.config import logger
+from languru.types.chat.completions import Message
 
 if TYPE_CHECKING:
     from openai.types.chat.chat_completion import ChatCompletion
@@ -100,6 +113,8 @@ def remove_punctuation(input_string: Text, extra_punctuation: Text = "") -> Text
 
 
 def ensure_openai_chat_completion_content(chat_completion: "ChatCompletion") -> Text:
+    """Ensure that the chat completion response content is returned."""
+
     chat_answer = chat_completion.choices[0].message.content
     if chat_answer is None:
         raise ValueError("Failed to get a response content from the OpenAI API.")
@@ -112,3 +127,30 @@ def ensure_list(value: Any) -> List:
     if value is None:
         return []
     return [value]
+
+
+def ensure_openai_chat_completion_message_params(
+    messages: Union[
+        Sequence["Message"],
+        Sequence[Dict[Text, Any]],
+        Sequence[ChatCompletionMessageParam],
+    ]
+) -> List[ChatCompletionMessageParam]:
+    """Ensure that the chat completion messages are in the correct format."""
+
+    _messages: List[ChatCompletionMessageParam] = []
+    for m in messages:
+        if isinstance(m, Message):
+            m_dict = m.model_dump()
+            if m_dict.get("role") not in ["user", "assistant", "system"]:
+                raise ValueError(f"Invalid role: {m_dict.get('role')}")
+            _messages.append(m_dict)  # type: ignore
+        elif isinstance(m, Dict):
+            if m.get("role") not in ["user", "assistant", "system"]:
+                raise ValueError(f"Invalid role: {m.get('role')}")
+            _messages.append(m)  # type: ignore
+        elif isinstance(m, ChatCompletionMessageParam.__args__):
+            _messages.append(m)
+        else:
+            raise ValueError(f"Invalid message type: {m}")
+    return _messages
