93a4acd041c9076f820c31960e555ff4284ffae0
Allen Chou <f1470891079@gmail.com>
2024-05-30 19:07:28 +0800

    Added `languru/utils/prompt.py`:
    - Introduces a new utility function: `ensure_chat_completion_message_param`.
    - Enforces correct formatting for messages used in OpenAI chat completions.
    - Raises a `ValueError` if no messages are provided.

---
+++ languru/utils/prompt.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict, List, Sequence, Text
+
+from openai.types.chat import ChatCompletionMessageParam
+
+
+def ensure_chat_completion_message_param(
+    messages: (
+        Sequence[ChatCompletionMessageParam]
+        | Sequence[Dict[Text, Any]]
+        | ChatCompletionMessageParam
+        | Dict[Text, Any]
+    )
+) -> List[ChatCompletionMessageParam]:
+    """Ensure the messages parameter is a list of ChatCompletionMessageParam objects."""
+
+    if not messages:
+        raise ValueError("The messages parameter is required.")
+
+    return list([messages] if isinstance(messages, Dict) else messages)  # type: ignore
