094bb20fe2b0484594dea91898656e1187a8a69c
Allen Chou <f1470891079@gmail.com>
2024-05-31 17:26:10 +0800

    Add streaming support for chat completions in GoogleOpenAI client
    - Add new test `test_google_openai_chat_completion_create_stream` to verify streaming functionality
    - Import `Stream` and `ChatCompletionChunk` types for type hinting and casting
    - Iterate over the stream, checking for `finish_reason` and `delta.content` in the response

--- tests/openai_plugins/test_google_openai.py
+++ tests/openai_plugins/test_google_openai.py
@@ -1,6 +1,8 @@
 from typing import cast
 
+from openai._streaming import Stream
 from openai.types.chat.chat_completion import ChatCompletion
+from openai.types.chat.chat_completion_chunk import ChatCompletionChunk
 
 from languru.openai_plugins.clients.google import GoogleOpenAI
 from languru.utils.prompt import ensure_chat_completion_message_params
@@ -24,3 +26,23 @@ def test_google_openai_chat_completion_create():
     )
     chat_res = cast(ChatCompletion, chat_res)
     assert chat_res.choices and chat_res.choices[0].message
+
+
+def test_google_openai_chat_completion_create_stream():
+    chat_stream = google_openai.chat.completions.create(
+        messages=ensure_chat_completion_message_params(
+            [
+                {"role": "system", "content": "Respond simple and concise."},
+                {"role": "user", "content": "Say Hi!"},
+            ]
+        ),
+        model=test_model_name,
+        temperature=0.0,
+        stream=True,
+    )
+    chat_stream = cast(Stream[ChatCompletionChunk], chat_stream)
+    for chunk in chat_stream:
+        if chunk.choices[0].finish_reason == "stop":
+            pass
+        else:
+            assert chunk.choices and chunk.choices[0].delta.content
