8d0135a2b35ad19778038e58240ec303c86bf4b9
Allen Chou <f1470891079@gmail.com>
2024-06-23 23:31:05 +0800

    Refactor audio API handlers to use OpenAI client dependency
    - Remove separate handlers for LLM and agent app types
    - Simplify request handling by using a single OpenAI client
    - Remove unused imports and redundant code
    - Update function signatures to include OpenAI client parameter
    - Adjust API endpoints to use the new OpenAI client dependency

--- languru/server/api/v1/audio.py
+++ languru/server/api/v1/audio.py
@@ -1,32 +1,14 @@
-import logging
-import math
-import random
-import time
-from typing import Text, cast
+from typing import Text
 
-from fastapi import (
-    APIRouter,
-    Body,
-    Depends,
-    File,
-    Form,
-    HTTPException,
-    Request,
-    UploadFile,
-)
+from fastapi import APIRouter, Body, Depends, File, Form, Request, UploadFile
 from fastapi.responses import StreamingResponse
+from openai import OpenAI
 from openai.types.audio import Transcription, Translation
 from pyassorted.asyncio.executor import run_func, run_generator
 
-from languru.exceptions import ModelNotFound
-from languru.server.config import (
-    AgentSettings,
-    AppType,
-    LlmSettings,
-    ServerBaseSettings,
-)
+from languru.server.config import ServerBaseSettings
 from languru.server.deps.common import app_settings
-from languru.server.utils.common import get_value_from_app
+from languru.server.deps.openai_clients import openai_clients
 from languru.types.audio import (
     AudioSpeechRequest,
     AudioTranscriptionRequest,
@@ -40,103 +22,18 @@ class AudioSpeechHandler:
     async def handle_request(
         self,
         request: "Request",
+        *args,
         audio_speech_request: "AudioSpeechRequest",
+        openai_client: "OpenAI",
         settings: "ServerBaseSettings",
         **kwargs,
     ) -> StreamingResponse:
-        if settings.APP_TYPE == AppType.llm:
-            settings = cast(LlmSettings, settings)
-            return await self.handle_llm(
-                request=request,
-                audio_speech_request=audio_speech_request,
-                settings=settings,
-                **kwargs,
-            )
-
-        if settings.APP_TYPE == AppType.agent:
-            settings = cast(AgentSettings, settings)
-            return await self.handle_agent(
-                request=request,
-                audio_speech_request=audio_speech_request,
-                settings=settings,
-                **kwargs,
-            )
-
-        # Not implemented or unknown app server type
-        raise HTTPException(
-            status_code=500,
-            detail=(
-                f"Unknown app server type: {settings.APP_TYPE}"
-                if settings.APP_TYPE
-                else "App server type not implemented"
-            ),
-        )
-
-    async def handle_llm(
-        self,
-        request: "Request",
-        audio_speech_request: "AudioSpeechRequest",
-        settings: "LlmSettings",
-        **kwargs,
-    ) -> StreamingResponse:
-        from languru.action.base import ActionBase
-
-        action: "ActionBase" = get_value_from_app(
-            request.app, key="action", value_typing=ActionBase
-        )
-
-        try:
-            audio_speech_request.model = action.get_model_name(
-                audio_speech_request.model
-            )
-        except ModelNotFound as e:
-            raise HTTPException(status_code=404, detail=str(e))
-
-        return StreamingResponse(
-            run_generator(
-                action.audio_speech,
-                **audio_speech_request.model_dump(exclude_none=True),
-            ),
-            media_type="audio/mpeg",
-        )
-
-    async def handle_agent(
-        self,
-        request: "Request",
-        audio_speech_request: "AudioSpeechRequest",
-        settings: "AgentSettings",
-        **kwargs,
-    ) -> StreamingResponse:
-        from openai import OpenAI
-
-        from languru.resources.model_discovery.base import ModelDiscovery
-
-        model_discovery: "ModelDiscovery" = get_value_from_app(
-            request.app, key="model_discovery", value_typing=ModelDiscovery
-        )
-        logger = logging.getLogger(settings.APP_NAME)
-
-        # Get model name and model destination
-        models = await run_func(
-            model_discovery.list,
-            id=audio_speech_request.model,
-            created_from=math.floor(time.time() - settings.MODEL_REGISTER_PERIOD),
-        )
-        if len(models) == 0:
-            raise HTTPException(
-                status_code=404,
-                detail=f"Model '{audio_speech_request.model}' not found",
-            )
-        model = random.choice(models)
-
         # Request audio speech
-        client = OpenAI(base_url=model.owned_by, api_key="NOT_IMPLEMENTED")
-        logger.debug(f"Using model '{model.id}' from '{model.owned_by}'")
-        with client.audio.speech.with_streaming_response.create(
+        with openai_client.audio.speech.with_streaming_response.create(
             **audio_speech_request.model_dump(exclude_none=True)
         ) as response:
             return StreamingResponse(
-                response.iter_bytes(),
+                run_generator(response.iter_bytes),  # type: ignore
                 media_type="audio/mpeg",
             )
 
@@ -146,96 +43,12 @@ class AudioTranscriptionHandler:
         self,
         request: "Request",
         audio_transcription_request: "AudioTranscriptionRequest",
+        openai_client: "OpenAI",
         settings: "ServerBaseSettings",
         **kwargs,
     ) -> Transcription:
-        if settings.APP_TYPE == AppType.llm:
-            settings = cast(LlmSettings, settings)
-            return await self.handle_llm(
-                request=request,
-                audio_transcription_request=audio_transcription_request,
-                settings=settings,
-                **kwargs,
-            )
-
-        if settings.APP_TYPE == AppType.agent:
-            settings = cast(AgentSettings, settings)
-            return await self.handle_agent(
-                request=request,
-                audio_transcription_request=audio_transcription_request,
-                settings=settings,
-                **kwargs,
-            )
-
-        # Not implemented or unknown app server type
-        raise HTTPException(
-            status_code=500,
-            detail=(
-                f"Unknown app server type: {settings.APP_TYPE}"
-                if settings.APP_TYPE
-                else "App server type not implemented"
-            ),
-        )
-
-    async def handle_llm(
-        self,
-        request: "Request",
-        audio_transcription_request: "AudioTranscriptionRequest",
-        settings: "LlmSettings",
-        **kwargs,
-    ) -> Transcription:
-        from languru.action.base import ActionBase
-
-        action: "ActionBase" = get_value_from_app(
-            request.app, key="action", value_typing=ActionBase
-        )
-
-        try:
-            audio_transcription_request.model = action.get_model_name(
-                audio_transcription_request.model
-            )
-        except ModelNotFound as e:
-            raise HTTPException(status_code=404, detail=str(e))
-
         return await run_func(
-            action.audio_transcriptions,
-            **audio_transcription_request.model_dump(exclude_none=True),
-        )
-
-    async def handle_agent(
-        self,
-        request: "Request",
-        audio_transcription_request: "AudioTranscriptionRequest",
-        settings: "AgentSettings",
-        **kwargs,
-    ) -> Transcription:
-        from openai import OpenAI
-
-        from languru.resources.model_discovery.base import ModelDiscovery
-
-        model_discovery: "ModelDiscovery" = get_value_from_app(
-            request.app, key="model_discovery", value_typing=ModelDiscovery
-        )
-        logger = logging.getLogger(settings.APP_NAME)
-
-        # Get model name and model destination
-        models = await run_func(
-            model_discovery.list,
-            id=audio_transcription_request.model,
-            created_from=math.floor(time.time() - settings.MODEL_REGISTER_PERIOD),
-        )
-        if len(models) == 0:
-            raise HTTPException(
-                status_code=404,
-                detail=f"Model '{audio_transcription_request.model}' not found",
-            )
-        model = random.choice(models)
-
-        # Request audio speech
-        client = OpenAI(base_url=model.owned_by, api_key="NOT_IMPLEMENTED")
-        logger.debug(f"Using model '{model.id}' from '{model.owned_by}'")
-        return await run_func(
-            client.audio.transcriptions.create,
+            openai_client.audio.transcriptions.create,
             **audio_transcription_request.model_dump(exclude_none=True),
         )
 
@@ -245,96 +58,12 @@ class AudioTranslationHandler:
         self,
         request: "Request",
         audio_translation_request: "AudioTranslationRequest",
+        openai_client: "OpenAI",
         settings: "ServerBaseSettings",
         **kwargs,
     ) -> Translation:
-        if settings.APP_TYPE == AppType.llm:
-            settings = cast(LlmSettings, settings)
-            return await self.handle_llm(
-                request=request,
-                audio_translation_request=audio_translation_request,
-                settings=settings,
-                **kwargs,
-            )
-
-        if settings.APP_TYPE == AppType.agent:
-            settings = cast(AgentSettings, settings)
-            return await self.handle_agent(
-                request=request,
-                audio_translation_request=audio_translation_request,
-                settings=settings,
-                **kwargs,
-            )
-
-        # Not implemented or unknown app server type
-        raise HTTPException(
-            status_code=500,
-            detail=(
-                f"Unknown app server type: {settings.APP_TYPE}"
-                if settings.APP_TYPE
-                else "App server type not implemented"
-            ),
-        )
-
-    async def handle_llm(
-        self,
-        request: "Request",
-        audio_translation_request: "AudioTranslationRequest",
-        settings: "LlmSettings",
-        **kwargs,
-    ) -> Translation:
-        from languru.action.base import ActionBase
-
-        action: "ActionBase" = get_value_from_app(
-            request.app, key="action", value_typing=ActionBase
-        )
-
-        try:
-            audio_translation_request.model = action.get_model_name(
-                audio_translation_request.model
-            )
-        except ModelNotFound as e:
-            raise HTTPException(status_code=404, detail=str(e))
-
         return await run_func(
-            action.audio_translations,
-            **audio_translation_request.model_dump(exclude_none=True),
-        )
-
-    async def handle_agent(
-        self,
-        request: "Request",
-        audio_translation_request: "AudioTranslationRequest",
-        settings: "AgentSettings",
-        **kwargs,
-    ) -> Translation:
-        from openai import OpenAI
-
-        from languru.resources.model_discovery.base import ModelDiscovery
-
-        model_discovery: "ModelDiscovery" = get_value_from_app(
-            request.app, key="model_discovery", value_typing=ModelDiscovery
-        )
-        logger = logging.getLogger(settings.APP_NAME)
-
-        # Get model name and model destination
-        models = await run_func(
-            model_discovery.list,
-            id=audio_translation_request.model,
-            created_from=math.floor(time.time() - settings.MODEL_REGISTER_PERIOD),
-        )
-        if len(models) == 0:
-            raise HTTPException(
-                status_code=404,
-                detail=f"Model '{audio_translation_request.model}' not found",
-            )
-        model = random.choice(models)
-
-        # Request audio speech
-        client = OpenAI(base_url=model.owned_by, api_key="NOT_IMPLEMENTED")
-        logger.debug(f"Using model '{model.id}' from '{model.owned_by}'")
-        return await run_func(
-            client.audio.translations.create,
+            openai_client.audio.translations.create,
             **audio_translation_request.model_dump(exclude_none=True),
         )
 
@@ -356,11 +85,13 @@ async def audio_speech(
             },
         },
     ),
+    openai_client=Depends(openai_clients.depends_openai_client),
     settings: ServerBaseSettings = Depends(app_settings),
 ) -> StreamingResponse:
     return await AudioSpeechHandler().handle_request(
         request=request,
         audio_speech_request=audio_speech_request,
+        openai_client=openai_client,
         settings=settings,
     )
 
@@ -376,6 +107,7 @@ async def audio_transcriptions(
     temperature: float = Form(None),
     timestamp_granularities: Text = Form(None),
     timeout: float = Form(None),
+    openai_client=Depends(openai_clients.depends_openai_client),
     settings: ServerBaseSettings = Depends(app_settings),
 ) -> Transcription:
     return await AudioTranscriptionHandler().handle_request(
@@ -392,6 +124,7 @@ async def audio_transcriptions(
                 "timeout": timeout,
             }
         ),
+        openai_client=openai_client,
         settings=settings,
     )
 
@@ -406,6 +139,7 @@ async def audio_translations(
     response_format: Text = Form(None),
     temperature: float = Form(None),
     timeout: float = Form(None),
+    openai_client=Depends(openai_clients.depends_openai_client),
     settings: ServerBaseSettings = Depends(app_settings),
 ) -> Translation:
     return await AudioTranslationHandler().handle_request(
@@ -421,5 +155,6 @@ async def audio_translations(
                 "timeout": timeout,
             }
         ),
+        openai_client=openai_client,
         settings=settings,
     )
